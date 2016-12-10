8179f70e61e9882ff0ca95704feb802cabb1e03f
Andrey Zavalin <garedok@gmail.com>
2016-12-10 21:19:48 +0300

    add hack to Gauss method (mb w/o it)

--- main.py
+++ main.py
@@ -9,10 +9,11 @@ import numpy as np
 
 # решение методом Гаусса
 def first_method():
-    step = 0.08
+    step = 0.05
     x = np.arange(0, 1, step)
     z = np.arange(0, 1, step)
-    X, Z = np.meshgrid(x, z)
+    # X, Z = np.meshgrid(x, z)
+    X, Z = np.meshgrid(x[1:len(x) - 1], z[1:len(z) - 1])
 
     z_len = len(z)
     x_len = len(x)
@@ -38,12 +39,17 @@ def first_method():
     print "With " + str(it) + " iterations"
 
     # dirty hack
-    ans_new[0] = ans_new[(x_len - 1) * z_len]
-    ans_new[x_len - 1] = ans_new[len(ans_new) - 1]
+    res = []
+    for i in xrange(1, z_len + 1):
+        for j in xrange(1, x_len + 1):
+            if i != 1 and i != z_len and j != 1 and j != x_len:
+                res.append(ans_new[matrix_utils.number(i, j, z_len)])
+    # ans_new[0] = ans_new[(x_len - 1) * z_len]
+    # ans_new[x_len - 1] = ans_new[len(ans_new) - 1]
     # ans_new[x_len - 2] = ans_new[len(ans_new) - 2]
     # ans_new[len(ans_new) - 1] = -500
 
-    y = np.array(ans_new)
+    y = np.array(res)
     Y = y.reshape(X.shape)
     draw_plot(X, Z, Y)
 
